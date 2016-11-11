e4ca146b9192c03772880078e169b04e0d5b5a5f
Robert Bailey (RIT Student) <rxb2890@ad.rit.edu>
2016-11-11 09:25:37 -0500

    All code updated onto working copy - currently bugged

--- GemJamLegends2k17/Assets/Scripts/PlayerScript.cs
+++ GemJamLegends2k17/Assets/Scripts/PlayerScript.cs
@@ -1,60 +1,65 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerScript : MonoBehaviour {
+public class PlayerScript : MonoBehaviour
+{
 
     public Faction faction;
-    public Unit[] units;
+    public Unit[] units = new Unit[6];
     public bool isTurn;
-    public Space[] capPoints;
     public int playerNum;
-	// Use this for initialization
-	void Start () {
-	    //populate units array with runners, brutes, and specials of faction type
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    GameManager gameMan;
+    private bool prevTurn;
+    Unit type;
 
-        do
+    // Use this for initialization
+    void Start()
+    {
+        //populate units array with runners, brutes, and specials of faction type
+        //3 runners, 2 brutes, 1 special
+        //boardManager.Spaces[0,0]
+        units[0] = new Unit(Space(0, 0), faction, type.Type.runner); //runner 1
+        units[1] = new Unit(Space(0, 4), faction, type.Type.runner); //runner 2
+        units[2] = new Unit(Space(0, 8), faction, type.Type.runner); //runner 3
+        units[3] = new Unit(Space(1, 3), faction, type.Type.brute); //brute 1
+        units[4] = new Unit(Space(1, 6), faction, type.Type.brute); //brute 2
+        units[5] = new Unit(Space(1, 4), faction, type.Type.special); //special
+        prevTurn = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (gameMan.currentPlayer == playerNum)
         {
+            isTurn = true;
+        }
+        else
+        {
+            isTurn = false;
+        }
+
+        if (isTurn == true && isTurn != prevTurn)
+        {
+            for (int i = 0; i < units.Length; i++)
+            {
+                units[i].WakeUp();
+            }
+
             //check for active units
             //if none are left, call endTurn()
-            //for(int i = 0; i < units.size(); i++)
-            controllingUnits();
+            for (int i = 0; i < units.Length; i++)
+            {
+                if (units[i].active == true)
+                {
+                    break;
+                }
+                endTurn();
+            }
+        }
 
-            //checks for turn skipping
-            passTurn();
-
-        } while (isTurn);
-	}
-
-    void controllingUnits()
-    {
-        //check for active units
-        //pressing directions on joystick or d-pad searches through units array
-        //if (left)
-        //{ int i = 0, units[i - 1] }
-
-        //if(right)
-        //{ int i = 0, units[i+1] }
-
-        //once player selects unit (hits interact button), call unitActions()
-        unitActions();
-    }
-
-    void unitActions()
-    {
-        //check for button input
-        //button corresponds to either move, attack, use abiity, or Pass(gem)
-        //pressing back button stops selection
-    }
-
-    void passTurn()
-    {
-        //timer holds gametime
-        //incremented every frame
-        //if (button) is held for x time, call endTurn()
+        prevTurn = isTurn;
     }
 
     void endTurn()
--- GemJamLegends2k17/Assets/Scripts/Unit.cs
+++ GemJamLegends2k17/Assets/Scripts/Unit.cs
@@ -7,45 +7,309 @@ public enum Faction
     SnowMen
 }
 
-public class Unit : MonoBehaviour {
+public enum Type
+{
+    Runner,
+    Brute,
+    Special
+}
 
-    protected int hp, maxKO, dex, move, xPos, yPos;
-    protected bool isMoved, abilityUsed, gemHeld;
+public class Unit : MonoBehaviour
+{
+
+    protected int hp, maxKO, dex, move, xPos, yPos, koturn;
+    protected bool isMoved = false, abilityUsed = false, gemHeld = false, isKOed = false, selected = false;
+    protected Space currSpace;
     private Faction fact;
+    private Type uType;
+    private boardManager board;
 
-    //Needed Parameter for enum
+    public Unit(Space starting, Faction group, Type unitType)
+    {
+        currSpace = starting;
+        xPos = starting.getX;
+        yPos = starting.getY;
+        koturn = 0;
+        fact = group;
+        uType = unitType;
+        switch (uType)
+        {
+            case Type.Runner:
+                if (fact == Faction.FunGuys)
+                {
+                    hp = 4;
+                    maxKO = 3;
+                    dex = 1;
+                }
+                else
+                {
+                    hp = 3;
+                    maxKO = 3;
+                    dex = 2;
+                }
+                move = 3;
+                break;
+            case Type.Brute:
+                hp = 6;
+                maxKO = 2;
+                dex = 1;
+                move = 2;
+                break;
+            case Type.Special:
+                if (fact == Faction.FunGuys)
+                {
+                    hp = 3;
+                    maxKO = 2;
+                    dex = 2;
+                }
+                else
+                {
+                    hp = 3;
+                    maxKO = 3;
+                    dex = 0;
+                }
+                move = 2;
+                break;
+        }
+    }
+
+    #region Parameters
+    public int HP
+    {
+        get
+        {
+            return hp;
+        }
+        set
+        {
+            hp = value;
+        }
+    }
+
+    public int MaxKO
+    {
+        get
+        {
+            return maxKO;
+        }
+    }
+
+    public int Dex
+    {
+        get
+        {
+            return dex;
+        }
+    }
+
+    public int Movement
+    {
+        get
+        {
+            return move;
+        }
+        set
+        {
+            move = value;
+        }
+    }
+
+    public int XPos
+    {
+        get
+        {
+            return xPos;
+        }
+        set
+        {
+            xPos = value;
+        }
+    }
+
+    public int YPos
+    {
+        get
+        {
+            return yPos;
+        }
+        set
+        {
+            yPos = value;
+        }
+    }
+
+    public int KOturn
+    {
+        get
+        {
+            return koturn;
+        }
+        set
+        {
+            koturn = value;
+        }
+    }
+
+    public bool IsMoved
+    {
+        get
+        {
+            return isMoved;
+        }
+        set
+        {
+            isMoved = value;
+        }
+    }
+
+    public bool AbilityUsed
+    {
+        get
+        {
+            return abilityUsed;
+        }
+        set
+        {
+            abilityUsed = value;
+        }
+    }
+
+    public bool GemHeld
+    {
+        get
+        {
+            return gemHeld;
+        }
+        set
+        {
+            gemHeld = value;
+        }
+    }
+
+    public bool IsKOed
+    {
+        get
+        {
+            return isKOed;
+        }
+        set
+        {
+            isKOed = value;
+        }
+    }
+
+    public bool Selected
+    {
+        get
+        {
+            return selected;
+        }
+        set
+        {
+            selected = value;
+        }
+    }
+
+    public Space CurrSpace
+    {
+        get
+        {
+            return currSpace;
+        }
+        set
+        {
+            currSpace = value;
+        }
+    }
+
     public Faction Fact
     {
         get
         {
             return fact;
         }
-        set
+    }
+
+    public Type UType
+    {
+        get
         {
-            fact = value;
+            return uType;
         }
     }
+
+    #endregion
 
     #region Methods
 
     //Moves the unit
-    public void Move()
+    public void Move(Space moveSpace)
     {
-        
+        if (ActionPossible(moveSpace, move))
+        {
+            currSpace = moveSpace;
+            xPos = moveSpace.getX;
+            yPos = moveSpace.getY;
+            isMoved = true;
+        }
     }
 
     //Takes an adjacent enemy Unit as parameter and deals damage to them
-    public void Attack(Unit enemy)
+    public void Attack()
     {
-
+        /*Space spaceToCheck;
+        if(xPos - 1 >= 0)
+        {
+            spaceToCheck = board.spaces[xPos - 1, yPos];
+            if (spaceToCheck.isOccupied)
+            {
+                spaceToCheck.getOccupier.GetComponent<Unit>().TakeDamage();
+            }
+        }
+        if(yPos - 1 >= 0)
+        {
+            spaceToCheck = board.spaces[xPos, yPos - 1];
+            if (spaceToCheck.isOccupied)
+            {
+                spaceToCheck.getOccupier.GetComponent<Unit>().TakeDamage();
+            }
+        }
+        if (xPos + 1 >= 0)
+        {
+            spaceToCheck = board.spaces[xPos + 1, yPos];
+            if (spaceToCheck.isOccupied)
+            {
+                spaceToCheck.getOccupier.GetComponent<Unit>().TakeDamage();
+            }
+        }
+        if (yPos + 1 >= 0)
+        {
+            spaceToCheck = board.spaces[xPos, yPos + 1];
+            if (spaceToCheck.isOccupied)
+            {
+                spaceToCheck.getOccupier.GetComponent<Unit>().TakeDamage();
+            }
+        }*/
     }
 
-    //Checks if space is moveable to
-    //Takes x and y of the grid spot being attempted to move to
-    //Returns true if within movement range, false if not within movement range
-    public bool MovePossible(int x, int y)
+    public void TakeDamage(int dam)
     {
-        return false;
+        hp -= dam;
+        if (hp == 0)
+        {
+            isKOed = true;
+        }
+    }
+
+    //Replacement for MovePossible that allows to test various other actions
+    //(Passing the gem and unit abilities, for example)
+    //Checks the Manhattan distance between current location and given space against the passed in max distance
+    //Returns false if out of 
+    public bool ActionPossible(Space actSpace, int dist)
+    {
+        if ((Mathf.Abs(xPos - actSpace.getX) + (Mathf.Abs(yPos - actSpace.getY))) > dist)
+            return false;
+        return true;
     }
 
     //Passes gem to teammate
@@ -53,21 +317,69 @@ public class Unit : MonoBehaviour {
     //Returns true if pass successful, false if unsuccessful
     public bool Pass(Unit other)
     {
+        if (other.Fact != this.fact)
+        {
+            other.TakeDamage(2);
+            //Gem falls on random space
+            //Waiting on RandomAdjacentSpace method in boardManager
+        }
+        else
+        {
+            if (other.AbilityUsed)
+                return false;
+            if (other.XPos == this.xPos + 1 || other.XPos == this.xPos - 1 || other.YPos == this.yPos + 1 || other.YPos == this.yPos - 1)
+                return true;
+            else
+            {
+                other.AbilityUsed = true;
+                int succRate = Random.Range(0, (other.Dex + 7));
+                if (succRate >= 5)
+                    return true;
+                //Gem falls on random space next to receiver
+            }
+        }
         return false;
+    }
+
+    public bool WakeUp()
+    {
+        if (koturn == maxKO)
+        {
+            koturn = 0;
+            isKOed = false;
+            return true;
+        }
+        else
+        {
+            int wakeChance = Random.Range(1, 7);
+            if (wakeChance >= 5)
+            {
+                koturn = 0;
+                isKOed = false;
+                return true;
+            }
+            else
+            {
+                koturn++;
+                return false;
+            }
+        }
     }
 
     //Method to be overloaded by children classes
     virtual public void Ability() { }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
     #endregion
 }
